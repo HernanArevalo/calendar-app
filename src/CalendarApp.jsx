82fbad731d9c1e552a10f361909754db2cb44b72
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-10-29 18:32:53 -0300

    first commit

---
+++ src/CalendarApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from './router'
+
+export const CalendarApp = () => {
+  return (
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  )
+}
