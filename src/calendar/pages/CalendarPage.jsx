da17ddd5f9f6c671be6248498f40a66a58a2a54f
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-10-29 21:41:24 -0300

    react big calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,51 @@
+
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+
 import { NavBar } from '../'
 
+
 export const CalendarPage = () => {
+
+  const locales = {
+    'en-US': enUS,
+  }
+
+  const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+  })
+
+  const events = [{
+    title: 'Cumpleaños',
+    notes: 'Comprar pastel',
+    start: new Date(),
+    end: addHours( new Date(), 2 ),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Fernando'
+    }
+
+  }]
+
   return (
     <>
       <NavBar />
+
+    <Calendar
+      localizer={ localizer }
+      events={ events }
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc( 100vh - 80px' }}
+    />
+
     </>
   )
 }
