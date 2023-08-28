7227b05256a4a3eed0fdabc92a93f05024ed8284
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-08-28 14:35:30 -0300

    test uiSlice

---
+++ tests/store/ui/uiSlice.test.js
@@ -0,0 +1,23 @@
+import { onCloseDateModal, onOpenDateModal, uiSlice } from "../../../src/store/ui/uiSlice"
+
+describe('Pruebas en uiSlice', () => { 
+    
+    test('Debe regresar el estado por defecto', () => { 
+    
+        expect( uiSlice.getInitialState().isDateModalOpen ).toBeFalsy();
+
+    }) 
+
+    test('should Debe cambiar el isDateModalOpen correctamente', () => {
+
+        let state = uiSlice.getInitialState();
+
+        state = uiSlice.reducer( state, onOpenDateModal() );
+        expect( state.isDateModalOpen ).toBeTruthy();
+
+        state = uiSlice.reducer( state, onCloseDateModal() );
+        expect( state.isDateModalOpen ).toBeFalsy();
+
+    })
+
+ })
