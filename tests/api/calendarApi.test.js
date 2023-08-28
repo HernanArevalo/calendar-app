685d1988b060860fdcdb5f475be03e071f9d9176
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-08-27 22:36:31 -0300

    test calendarApi

--- tests/api/calendarApi.test.js
+++ tests/api/calendarApi.test.js
@@ -10,4 +10,17 @@ describe('Pruebas en calendarApi', () => {
         expect( calendarApi.defaults.baseURL ).toBe( process.env.VITE_API_URL );
 
     });
+
+    test('Debe tener el x-token en el header de todas las peticiones', async () => { 
+        
+        const token = 'ABC-123';
+        localStorage.setItem('token', token);
+
+        const res = await calendarApi.get('/auth');
+
+        expect( res.config.headers['x-token']).toBe(token);
+
+
+     })
+
 });
