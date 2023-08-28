6adb20bce86019560c85433fef1ffe2691adde5f
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-08-28 16:37:27 -0300

    test authSlice

---
+++ tests/fixtures/authStates.js
@@ -0,0 +1,24 @@
+
+export const initialState = {
+    status: 'checking',
+    user: {},
+    errorMessage: undefined,
+
+}
+
+export const authenticatedState = {
+    status: 'authenticated',
+    user: {
+        uid: 'ABC123',
+        name: 'Fernando'
+    },
+    errorMessage: undefined,
+
+}
+
+export const notAuthenticatedState = {
+    status: 'not-authenticated',
+    user: {},
+    errorMessage: undefined,
+
+}
---
+++ tests/store/auth/authSlice.test.js
@@ -0,0 +1,50 @@
+import { authSlice, clearErrorMessage, onLogin, onLogout } from "../../../src/store/auth/authSlice"
+import { authenticatedState, initialState } from "../../fixtures/authStates"
+import { testUserCredentials } from "../../fixtures/testUse";
+
+describe('Pruebas en authSlice', () => { 
+    
+    test('Debe regresar el estado inicial', () => { 
+        
+        expect( authSlice.getInitialState() ).toEqual( initialState );
+
+    });
+
+    test('Debe realizar el login', () => { 
+        
+        const state = authSlice.reducer( initialState, onLogin( testUserCredentials ) );
+
+        expect( state ).toEqual({
+                                    status: 'authenticated',
+                                    user: testUserCredentials,
+                                    errorMessage: undefined
+                                });
+
+     })
+
+     test('Debe realizar el logout', () => { 
+        
+        const errorMessage = 'Invalid credentials'
+        const state = authSlice.reducer( authenticatedState, onLogout( errorMessage ) );
+
+        expect( state ).toEqual({
+                                    status: 'not-authenticated',
+                                    user: {},
+                                    errorMessage: errorMessage
+                                });
+      });
+
+      test('Debe limpiar el errorMessage', () => { 
+        
+        const errorMessage = 'Invalid credentials'
+        
+        let state = authSlice.reducer( authenticatedState, onLogout( errorMessage ) );
+        expect( state.errorMessage ).toBe( errorMessage );
+
+        state = authSlice.reducer( authenticatedState, clearErrorMessage() );
+        expect( state.errorMessage ).toBe( undefined );
+
+        
+
+      });
+ })
