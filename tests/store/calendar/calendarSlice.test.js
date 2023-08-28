92688dd76991aa1b6d2bb243e98d1ad2d1e7c327
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-08-28 17:54:35 -0300

    test calendarSlice

--- tests/store/calendar/calendarSlice.test.js
+++ tests/store/calendar/calendarSlice.test.js
@@ -1,5 +1,5 @@
-import { calendarSlice, onAddNewEvent, onSetActiveEvent, onUpdateEvent } from "../../../src/store/calendar/calendarSlice"
-import { calendarWithActiveEventsState, calendarWithEventsState, events, initialState } from "../../fixtures/calendarStates";
+import { calendarSlice, onAddNewEvent, onDeleteEvent, onLoadEvents, onLogoutCalendar, onSetActiveEvent, onUpdateEvent } from "../../../src/store/calendar/calendarSlice"
+import { calendarWithActiveEventState, calendarWithEventsState, events, initialState } from "../../fixtures/calendarStates";
 
 describe('pruebas en calendarSlice', () => { 
     
@@ -12,10 +12,12 @@ describe('pruebas en calendarSlice', () => {
 
         test('onSetActiveEvent debe activar el evento', () => { 
         
-        const state = calendarSlice.reducer( calendarWithActiveEventsState, onSetActiveEvent( events[0] ) );
+        const state = calendarSlice.reducer( calendarWithActiveEventState, onSetActiveEvent( events[0] ) );
 
         expect( state.activeEvent ).toEqual( events[0] );
+
         })
+
 
         test('onAddNewEvent debe agregar el evento', () => { 
             
@@ -33,6 +35,7 @@ describe('pruebas en calendarSlice', () => {
         
         });
 
+
         test('onUpdateEvent debe actualizar el evento', () => { 
             
             const updatedEvent = {
@@ -47,6 +50,44 @@ describe('pruebas en calendarSlice', () => {
 
             expect( state.events ).toContain( updatedEvent );
 
+        });
 
-         })
+        
+        test('onDeleteEvent debe borrar el evento activo', () => { 
+            const eventToDelete = {
+                "id": "1",
+                "start": new Date('2022-10-21 13:00:00'),
+                "end": new Date( '2022-10-21 15:00:00' ),
+                "title": "Cumpleaños del jefe",
+                "notes": "Comprar torta",
+            }
+
+            const state = calendarSlice.reducer( calendarWithActiveEventState, onDeleteEvent(eventToDelete ))
+
+            expect( state.activeEvent ).toBe( null );
+            expect( state.events ).not.toContain( eventToDelete );
+
+        })
+        test('onLoadEvents debe establecer los eventos', () => { 
+
+            
+            const state = calendarSlice.reducer( initialState, onLoadEvents( events ));
+    
+            expect( state.events ).toEqual( events )
+            
+
+        })
+        test('onLogoutCalendar debe limpiar el estado', () => { 
+
+            const state = calendarSlice.reducer( calendarWithActiveEventState, onLogoutCalendar() );
+            expect( state ).toEqual( initialState )
+
+
+            expect( state.isLoadingEvents ).toBeTruthy();
+            expect( state.events ).toEqual([]);
+            expect( state.activeEvent ).toBe( null );
+
+
+
+        })
  })
